d1f9ccce4bfed4b78dbdec1f854bd4a13e8cb51e
Ilia_Nikoloaev <n.ilya@yandex.ru>
2025-04-26 21:07:55 +0300

    Add elements.

--- product/common/equipment.py
+++ product/common/equipment.py
@@ -7,14 +7,41 @@ class KitchenEquipment:
 
 
 class Dishwasher(KitchenEquipment):
-    def __init__(self, is_built_in, width, name=None):
+    def __init__(self, is_built_in, width, name):
         super().__init__(name=name, width=width, depth=None, height=None)
         self.is_built_in: bool = is_built_in
 
 
 class Oven(KitchenEquipment):
-    def __init__(self, width, height, name=None):
+    def __init__(self, width, height, name):
         super().__init__(name=name, width=width, depth=None, height=height)
 
 
+class Cooktop(KitchenEquipment):
+    def __init__(self, name, width, depth, number_hobs):
+        super().__init__(name=name, width=width, depth=depth, height=None)
+        self.number_hobs: int = number_hobs
 
+
+class Hood(KitchenEquipment):
+    def __init__(self, name, mounting_type, width, depth, height ):
+        super().__init__(name=name, width=width, depth=depth, height=height)
+        self.mounting_type: str = mounting_type
+
+
+class Washer(KitchenEquipment):
+    def __init__(self, name, is_built_in, width, depth, height ):
+        super().__init__(name=name, width=width, depth=depth, height=height)
+        self.mounting_type: bool = is_built_in
+
+
+class Microwave(KitchenEquipment):
+    def __init__(self, name, is_built_in, width, depth, height ):
+        super().__init__(name=name, width=width, depth=depth, height=height)
+        self.mounting_type: bool = is_built_in
+
+
+class Fridge(KitchenEquipment):
+    def __init__(self, name, is_built_in, width, depth, height ):
+        super().__init__(name=name, width=width, depth=depth, height=height)
+        self.mounting_type: bool = is_built_in
---
+++ product/details/plates.py
@@ -0,0 +1,8 @@
+class Panel:
+    def __init__(self, name, color, thickness, width, depth, height):
+        self.name: str = name
+        self.color:str = color
+        self. thickness: int = thickness
+        self.width: int = width
+        self.depth: int = depth
+        self.height: int = height
